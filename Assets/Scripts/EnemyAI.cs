ea56bffcac72a65908134cb6a5cbbad05dcb0f19
Dominic <dominic.wenzel@mailbox.org>
2023-08-23 22:27:50 +0200

    adding zombie enemy with walk and death animation

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,30 +8,39 @@ public class EnemyAI : MonoBehaviour
     public int damage = 10;
 
     public int maxHits = 5; // Maximum number of hits before enemy is destroyed
-   
 
-     private int hitCount = 0;
+    public float rotationSpeed = 5f; // Adjust as needed
 
+    private int hitCount = 0;
 
+    private Animator animator;
     private Transform playerTransform;
     private bool canAttack = true;
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        animator.SetBool("isRunning", true);
     }
 
     private void Update()
     {
         // Move towards the player
-        Vector3 directionToPlayer = playerTransform.position - transform.position;
-        transform.Translate(directionToPlayer.normalized * moveSpeed * Time.deltaTime);
-
+        // Vector3 directionToPlayer = playerTransform.position - transform.position;
+        //transform.Translate(directionToPlayer.normalized * moveSpeed * Time.deltaTime);
+        animator.SetBool("isRunning", true);
+        RotateTowardsPlayer();
         // Check distance to player for attacking
-        if (directionToPlayer.magnitude <= attackRange && canAttack)
-        {
-            AttackPlayer();
-        }
+        // if (directionToPlayer.magnitude <= attackRange && canAttack)
+        // {
+
+        // AttackPlayer();
+        //  }
+        //  else
+        //  {
+        //       animator.SetBool("isRunning", true);
+        //  }
     }
 
     private void AttackPlayer()
@@ -47,7 +56,7 @@ public class EnemyAI : MonoBehaviour
     {
         canAttack = true;
     }
-     private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
@@ -55,11 +64,23 @@ public class EnemyAI : MonoBehaviour
 
             if (hitCount >= maxHits)
             {
-                Destroy(gameObject); // Destroy the enemy if hit count exceeds maxHits
+                 animator.SetBool("isDead", true);
+               // Destroy(gameObject); // Destroy the enemy if hit count exceeds maxHits
             }
-            
+
 
             Destroy(collision.gameObject); // Destroy the bullet
         }
     }
+    private void RotateTowardsPlayer()
+    {
+        if (playerTransform != null)
+        {
+            Vector3 directionToPlayer = playerTransform.position - transform.position;
+            directionToPlayer.y = 0f; // Ensure no vertical rotation
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
 }
+
